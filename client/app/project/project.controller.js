0c3efa73c8a92ac62088cb408410dbfdf2f3821b
Pascal Crenzin <pascal.crenzin@sap.com>
2016-02-14 14:16:01 -0800

    little changes

--- client/app/project/project.controller.js
+++ client/app/project/project.controller.js
@@ -9,66 +9,87 @@ angular.module('easySpinUpApp')
 
         $scope.labels = ["Success", "Untested", "Failed"];
         $scope.data = [30, 12, 5];
-    
-    $http.get("/api/netapp").success(function (data) {console.log(data)});
-    
-    
-    $scope.sampleData = [
- {
-   "name": "hourly.2016-02-14_2005",
-   "accessTime": 1455480300000,
-   "label": null
+
+        $http.get("/api/netapp").success(function (data) {
+            console.log(data)
+        });
+
+
+
+
+        $scope.sampleData = [
+            {
+                "name": "hourly.2016-02-14_2005",
+                "accessTime": 1455480300000,
+                "label": null
  },
- {
-   "name": "hourly.2016-02-14_1905",
-   "accessTime": 1455476700000,
-   "label": null
+            {
+                "name": "hourly.2016-02-14_1905",
+                "accessTime": 1455476700000,
+                "label": null
  },
- {
-   "name": "hourly.2016-02-14_1805",
-   "accessTime": 1455473100000,
-   "label": null
+            {
+                "name": "hourly.2016-02-14_1805",
+                "accessTime": 1455473100000,
+                "label": null
  },
- {
-   "name": "clone_volume3.0",
-   "accessTime": 1455472479000,
-   "label": null
+            {
+                "name": "clone_volume3.0",
+                "accessTime": 1455472479000,
+                "label": null
  },
- {
-   "name": "hourly.2016-02-14_1205",
-   "accessTime": 1455451500000,
-   "label": null
+            {
+                "name": "hourly.2016-02-14_1205",
+                "accessTime": 1455451500000,
+                "label": null
  },
- {
-   "name": "clone_volume1_copy5.0",
-   "accessTime": 1455451295000,
-   "label": null
+            {
+                "name": "clone_volume1_copy5.0",
+                "accessTime": 1455451295000,
+                "label": null
  },
- {
-   "name": "clone_volume1_copy3.0",
-   "accessTime": 1455450975000,
-   "label": null
+            {
+                "name": "clone_volume1_copy3.0",
+                "accessTime": 1455450975000,
+                "label": null
  },
- {
-   "name": "clone_volume1_copy.0",
-   "accessTime": 1455450681000,
-   "label": null
+            {
+                "name": "clone_volume1_copy.0",
+                "accessTime": 1455450681000,
+                "label": null
  },
- {
-   "name": "hourly.2016-02-14_1105",
-   "accessTime": 1455447900000,
-   "label": null
+            {
+                "name": "hourly.2016-02-14_1105",
+                "accessTime": 1455447900000,
+                "label": null
  },
- {
-   "name": "hourly.2016-02-14_1005",
-   "accessTime": 1455444300000,
-   "label": null
+            {
+                "name": "hourly.2016-02-14_1005",
+                "accessTime": 1455444300000,
+                "label": null
  }
 ];
-    
-    $scope.getStuff = function() {
-        $http.get("https://54.153.6.84:443/occm/api/vsa/volumes/VsaWorkingEnvironment-Wo9as61C/svm_EasySpinUp/volume1/snapshots");
-    };
+
+        $scope.getStuff = function () {
+            $http.get("https://54.153.6.84:443/occm/api/vsa/volumes/VsaWorkingEnvironment-Wo9as61C/svm_EasySpinUp/volume1/snapshots");
+        };
+
+        $http({
+            url: "https://54.153.6.84/occm/api/vsa/volumes/VsaWorkingEnvironment-Wo9as61C/svm_EasySpinUp/volume1/clone",
+            method: "POST",
+            headers: {
+                "Cookie": "rememberMe=bq7nIiZgZtPXyhCUV6bE4rVMEm9a8IIsh73/pdBDptmpX0moo28QXMuAqf1WEwuhYN2VXndXq+fgviXR/BisTvXrYEVYNII12LY6YDHcD9QDUb7C2atMUe02lfi+za99ZoAEM3WIi6RXgp7QzQYChLLxtvvTmRY2tlno54V+gpxeq4oBY21B1PYWLoZOw1fPOuSVZSPUTLNMa8QEZpf5tHQXwztsPqmgaPMAvuaTCzyUhFCE0X/yCmDIiZSrQz/ifzuEPL98qYUzC7iQRNIZl4WLKb3KnAgf9j27fS9Guz4IFhq/CQ6Q5J5CelwzyP9WeKdjHFYELhgBt6ux3xwA/JtHl4mC1S3n+N0A/OjcZ1l67ayL7RfQy4ntZ8ZeDrnkjcchYrgG+rOtVxIftcmUak21cZthsYz2NIZMnBr7le+0y+0ka1X56g1cWohaU0bZEQT/AOocQhcjqg10wSq/Ue4f1Z++bA7Kabfz8QmTwQsAkoBMd3irSBzmf0zJ0/6g",
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            crossOrigin: true,
+            contentType: "application/json",
+            data: JSON.stringify({
+                "newVolumeName": "myTestVoume"
+            }),
+        });
+
+
 
 
 
