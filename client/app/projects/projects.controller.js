8f01d0946b437a978cb28cf3983e7605d6aff973
Michael Janke <micha@jankeweb.de>
2016-02-14 14:10:06 -0800

    layout

--- client/app/projects/projects.controller.js
+++ client/app/projects/projects.controller.js
@@ -9,7 +9,9 @@ angular.module('easySpinUpApp')
       socket.syncUpdates('projects', $scope.projects);
         
     $scope.junctionPath = "/Volume1/data";
-        
+        $scope.labels = ["Success", "Untested", "Failed"];
+        var bla = [[30, 12, 5],[80, 12, 5],[95, 0, 5]];
+        $scope.data = function(index) {return bla[index]};   
         $scope.goto = function (project_id) {
             $state.go('project', {id: project_id});
         }
